72d8cefd3c2674acf3a98d5b7e029ac4d5417127
Christoph Meissl <meissl.christoph@gmail.com>
2021-05-08 01:50:52 +0200

    Button  Component

--- src/App.svelte
+++ src/App.svelte
@@ -1,44 +1,57 @@
 <script>
-	import Button from "./Button.svelte";
-	let button = {
-		icon: "account_circle",
-		size: "large",
-		label: "User",
-	};
+  import Button from "./Button.svelte";
+  let button = {
+    icon: "account_circle",
+    size: "large",
+    label: "User",
+    disabled: true,
+    fullWidth: true,
+  };
 </script>
 
 <svelte:head>
-	<link
-		href="https://fonts.googleapis.com/icon?family=Material+Icons"
-		rel="stylesheet"
-	/>
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
 </svelte:head>
 
 <main>
-	<Button {...button} />
-	<Button icon="lock" color="white" backgroundColor="maroon" size="medium" label="Sperren" />
-	<Button icon="lock" color="white" backgroundColor="maroon" size="small" />
-	<Button color="white" backgroundColor="black" size="medium" label="Help!" />
+  <Button {...button} />
+  <Button
+    icon="lock"
+    color="white"
+    backgroundColor="maroon"
+    size="medium"
+    label="Sperren"
+    fullWidth={true}
+    disabled={true}
+  />
+  <Button
+    icon="local_fire_department"
+    color="white"
+    backgroundColor="maroon"
+    size="large"
+    disabled={true}
+  />
+  <Button color="white" backgroundColor="black" size="medium" label="Help!" disabled={true} />
+  <Button label="Set" size="small" disabled={true} />
+  <Button label="Close" size="small" />
+  <Button label="Open" size="small" />
+  <Button label="Set" size="large" />
+  <Button label="Close" />
+  <Button label="Open" />
+  <Button label="Shit" fullWidth={true} size="large" />
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
--- src/Button.svelte
+++ src/Button.svelte
@@ -1,71 +1,84 @@
 <script>
-    import Icon from "./Run.svelte";
+  import Icon from "./Run.svelte";
 
-    export let icon = false;
-    export let label = "";
-    export let backgroundColor = "white";
-    export let color = "#363636";
-    export let size = "medium";
-    export let borderStyle = "solid";
-    export let borderColor = "#dbdbdb";
+  export let icon = false;
+  export let label = "";
+  export let backgroundColor = "white";
+  export let color = "#363636";
+  export let size = "medium";
+  export let borderStyle = "solid";
+  export let borderColor = "#dbdbdb";
+  export let disabled = false;
+  export let fullWidth = false;
 
-    let iconOnly = (icon !== false && label === '') ? true : false;
-    let labelOnly = (label !== '' && icon === false) ? true : false;
-
+  let iconOnly = icon !== false && label === "" ? true : false;
 </script>
 
-<div
-    class="button {size}"
-    style="background-color: {backgroundColor}; color: {color}; border-style: {borderStyle}; border-color: {borderColor};"
+<button
+  class="button {size}"
+  class:full-width={fullWidth}
+  style="background-color: {backgroundColor}; color: {color}; border-style: {borderStyle}; border-color: {borderColor};"
+  {disabled}
 >
-    {#if icon}
-        <span class="material-icons icons {size}" class:icon-only={iconOnly}>{icon}</span>
-    {/if}
-    {#if label !== ""}
-        <span class="label">{label}</span>
-    {/if}
-</div>
+  {#if icon}
+    <span class="material-icons icons {size}" class:icon-only={iconOnly}>{icon}</span>
+  {/if}
+  {#if label !== ""}
+    <span class="label">{label}</span>
+  {/if}
+</button>
 
 <style>
-    .button {
-        display: inline-flex;
-        border-width: 1px;
-        cursor: pointer;
-        justify-content: center;
-        text-align: center;
-        white-space: nowrap;
-        padding: .6rem;
-    }
-    .icons,
-    .label {
-        display: inline-flex;
-        align-items: center;
-        justify-content: center;
-    }
-    .icons{
-        margin-right: .3rem;
-    }
-    .label{
-        padding-bottom: 2px;
-    }
-    .icon-only{
-        margin-left: 0 !important;
-        margin-right: 0 !important;
-    }
-    .large {
-        border-radius: 0.7rem;
-        font-size: 1.3rem;
-        font-weight: 500;
-        letter-spacing: 0.1rem;
-    }
-    .medium {
-        border-radius: 0.5rem;
-        font-size: 1rem;
-        font-weight: 400;
-    }
-    .small {
-        border-radius: 0.3rem;
-        font-size: 0.75rem;
-        font-weight: 400;
-    }
+  .button {
+    display: inline-flex;
+    border-width: 1px;
+    cursor: pointer;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    white-space: nowrap;
+    padding: 0.6rem;
+  }
+  .icons,
+  .label {
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .icons {
+    margin-right: 0.3rem;
+  }
+  .label {
+    padding-bottom: 2px;
+  }
+  .icon-only {
+    margin-left: 0 !important;
+    margin-right: 0 !important;
+  }
+  .large {
+    border-radius: 0.7rem;
+    font-size: 1.3rem;
+    font-weight: 500;
+    letter-spacing: 0.1rem;
+  }
+  .medium {
+    border-radius: 0.5rem;
+    font-size: 1rem;
+    font-weight: 400;
+  }
+  .small {
+    border-radius: 0.3rem;
+    font-size: 0.75rem;
+    font-weight: 400;
+  }
+  .button[disabled] {
+    background-color: #fff;
+    border-color: #dbdbdb;
+    box-shadow: none;
+    opacity: 0.5;
+    cursor: not-allowed;
+  }
+  .full-width {
+    width: 100%;
+  }
 </style>
